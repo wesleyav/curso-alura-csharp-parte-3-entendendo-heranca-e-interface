111b69cebb075b3dda7d3d9d0e124427adf25f9d
Wesley <wesley.contato@hotmail.com>
2020-11-13 19:28:04 -0300

    Atalhos no VisualStudio e classe Funcionario

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+
+        }
+    }
+}
