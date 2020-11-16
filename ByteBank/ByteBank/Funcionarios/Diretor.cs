074740f6c7f534dfabc3d144b596896dc866378a
Wesley <wesley.contato@hotmail.com>
2020-11-16 07:57:14 -0300

    Problema da herança múltipla

--- ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor : Autenticavel
+    public class Diretor : Autenticavel, Funcionario
     {
         public Diretor(string cpf) : base(5000, cpf)
         {
